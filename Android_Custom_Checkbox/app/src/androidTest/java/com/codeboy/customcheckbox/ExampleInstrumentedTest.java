327507947ecb9d2f3e42b9dea13e7954917475bb
jason <yaoshihyu@gmail.com>
2021-08-08 02:30:14 +0800

    updategg

--- Android_Custom_Checkbox/app/src/androidTest/java/com/codeboy/customcheckbox/ExampleInstrumentedTest.java
+++ Android_Custom_Checkbox/app/src/androidTest/java/com/codeboy/customcheckbox/ExampleInstrumentedTest.java
@@ -1,15 +1,10 @@
 package com.codeboy.customcheckbox;
-
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -23,7 +18,6 @@ public class ExampleInstrumentedTest
     {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
         assertEquals("com.codeboy.customcheckbox", appContext.getPackageName());
     }
 }
